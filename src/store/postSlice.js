9168b2579ab7e9c5060271e8198e9b801f4655e1
Subhan Rahiman <subhanrahiman7@gmail.com>
2025-08-11 11:00:40 +0530

    removing comments

--- src/store/postSlice.js
+++ src/store/postSlice.js
@@ -1,69 +1,50 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// Think of this as the initial box where we store all our post-related data
 const initialState = {
-    // List of all blog posts
     posts: [],
-    // Are we loading data? (true/false)
     loading: false,
-    // Any error message if something goes wrong
     error: null,
-    // The post we're currently viewing/editing
     currentPost: null
 }
 
-// This is our post manager (slice) that handles all post-related actions
 const postSlice = createSlice({
-    name: "post", // Just a name to identify this slice
-    initialState, // Our initial data from above
-
-    // These are all the actions we can do with our posts
+    name: "post",
+    initialState,
     reducers: {
-        // When we get posts from the server, save them here
         setPosts: (state, action) => {
-            state.posts = action.payload; // Save the posts
-            state.loading = false;        // We're done loading
-            state.error = null;           // Clear any errors
+            state.posts = action.payload;
+            state.loading = false;
+            state.error = null;
         },
 
-        // Show/hide loading spinner
         setLoading: (state, action) => {
-            state.loading = action.payload; // true = show loading, false = hide loading
+            state.loading = action.payload;
         },
 
-        // If something goes wrong, save the error message
         setError: (state, action) => {
-            state.error = action.payload;  // Save error message
-            state.loading = false;         // Stop loading
+            state.error = action.payload;
+            state.loading = false;
         },
 
-        // When viewing a single post, save it here
         setCurrentPost: (state, action) => {
             state.currentPost = action.payload;
         },
 
-        // When creating a new post
         addPost: (state, action) => {
-            state.posts.push(action.payload); // Add new post to our list
+            state.posts.push(action.payload);
         },
 
-        // When editing a post
         updatePost: (state, action) => {
-            // Find the post we want to update
             const index = state.posts.findIndex(post => post.$id === action.payload.$id);
             if (index !== -1) {
-                // Replace the old post with the updated one
                 state.posts[index] = action.payload;
             }
         },
 
-        // When deleting a post
         deletePost: (state, action) => {
-            // Remove the post with the given ID
             state.posts = state.posts.filter(post => post.$id !== action.payload);
         },
 
-        // Reset everything back to initial state
         clearPosts: (state) => {
             state.posts = [];
             state.currentPost = null;
@@ -73,17 +54,15 @@ const postSlice = createSlice({
     }
 });
 
-// Export all our actions so we can use them in our components
 export const {
-    setPosts,      // Save all posts
-    setLoading,    // Show/hide loading
-    setError,      // Save error message
-    setCurrentPost, // Save current post
-    addPost,       // Create new post
-    updatePost,    // Edit post
-    deletePost,    // Delete post
-    clearPosts     // Reset everything
+    setPosts,
+    setLoading,
+    setError,
+    setCurrentPost,
+    addPost,
+    updatePost,
+    deletePost,
+    clearPosts
 } = postSlice.actions;
 
-// Export the reducer for our store
 export default postSlice.reducer;
